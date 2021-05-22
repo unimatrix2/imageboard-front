6ecf6913f799327c15babc9904d9d64c41805e9c
unimatrix2 <dnny.nis@gmail.com>
2021-05-22 00:24:17 -0300

    implemented ssr

--- pages/[abbr].jsx
+++ pages/[abbr].jsx
@@ -8,29 +8,40 @@ import {
   Typography,
   Container,
 } from "@material-ui/core";
+import axios from "axios";
 import useContainerSize from "../hooks/containerSize";
 import PersistentDrawer from "../components/PersistentDrawer";
 import { appTheme } from "../utils/theme";
 import { calcAppBarSize, handleOverflow } from "../utils/cssUtils";
-// Needs change. Prop getting should happen at each request,
-// fetching the board data, posts and replies
-export const getServerSideProps = (context) => {
+import { BorderAllTwoTone } from "@material-ui/icons";
+
+// This is using SSR right now, but ideally
+// should be incremental static generation
+// to limit the number of requests to the server.
+
+// SSR was adopted for the lack of a better solution.
+// This will be changed
+export const getServerSideProps = async (context) => {
+  const board = await axios.get(
+    `${process.env.NEXT_PUBLIC_BOARD_ROOT}/${context.query.abbr}`,
+    { withCredentials: true }
+  );
   return {
     props: {
-      data: context.query,
+      board: board.data
     },
   };
 };
 
-export default function Board({ data }) {
+export default function Board({ board }) {
   useDisplay();
   const { state } = useContext(Context);
   const customTheme = createMuiTheme(appTheme);
-
   return (
     <ThemeProvider theme={customTheme}>
       {state?.deviceWindow ? <PersistentDrawer /> : ""}
-      {state?.deviceWindow ? <Typography>{data.abbr}</Typography> : ""}
+      {state?.deviceWindow ? <Typography>{board.abbr}</Typography> : ""}
+      {state?.deviceWindow ? <Typography>{board.title}</Typography> : ""}
     </ThemeProvider>
   );
 }
